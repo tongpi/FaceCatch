037a8370b140c2e4fe8c3847aebf47d1eca794f9
sunxingba <sunxingba@e-u.cn>
2019-03-14 18:37:02 +0800

    样式修改及文件名修改

--- facecatch/views.py
+++ facecatch/views.py
@@ -3,9 +3,9 @@ from flask import render_template
 blueprint = flask.Blueprint(__name__, __name__)
 
 
-@blueprint.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
+# @blueprint.route('/', methods=['GET', 'POST'])
+# def home():
+#     return render_template('base.html')
 
 
 
--- settings.py
+++ settings.py
@@ -6,13 +6,13 @@ import os
 
 FLASK_SECRET_KEY = 'face_catch'
 
-SQLALCHEMY_DATABASE_URI = os.environ.get("FACECATCH_DATABASE_URL", "postgresql:///postgres")
+SQLALCHEMY_DATABASE_URI = os.environ.get("FACECATCH_DATABASE_URL", "postgresql://postgres:a1b2c3@localhost/facecatch")
 SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS", False)
 
 # DIGITS地址的配置
 DISCERN_IP = os.environ.get('DISCERN_ADDRESS', '192.168.15.100:50333')
 DISCERN_URL = 'http://{ip}/models/images/classification/classify_one.json'.format(ip=DISCERN_IP)
-DIGITS_JOB_ID = os.environ.get('DIGITS_JOB_ID', '20190312-031331-71ad')
+DIGITS_JOB_ID = os.environ.get('DIGITS_JOB_ID', '20190314-060225-a6b2')
 
 
 
