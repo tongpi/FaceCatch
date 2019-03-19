81c1fc646ff74d258b3a4ff8705d1eac62b6256b
tinyEnd <41897529+tinyEnd@users.noreply.github.com>
2019-03-19 15:04:33 +0800

    人脸识别0.0.2b 合并代码

--- facecatch/views.py
+++ facecatch/views.py
@@ -2,27 +2,25 @@ import flask
 from flask import Response, jsonify, session, request
 from facecatch.utils import get_all_models
 import json
-# from app import cas
+
 
 blueprint = flask.Blueprint(__name__, __name__)
 
 
 @blueprint.route('/get_models', methods=['POST', 'GET'])
 def get_models():
+    """获取模型列表，将默认模型存储至session"""
     model_dict = get_all_models()
-    model_list = list(model_dict.keys())
 
     if not session.get('model_id'):
-        session['model_id'] = model_list[0]
+        session['model_id'] = list(model_dict.keys())[0]
 
     return jsonify(model_dict)
 
 
 @blueprint.route('/set_session', methods=['POST', 'GET'])
 def set_model_session():
+    """跟随模型下拉框设置系统默认模型"""
     model_id = json.loads(request.get_data().decode())['model_id']
-    print(model_id)
-    print(session.get('model_id'))
     session['model_id'] = model_id
-    print(session.get('model_id'))
     return 'success'
